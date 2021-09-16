e81eea16fecddaf2385275a4cf37715ce355809e
petracili <petracili7@gmail.com>
2021-09-16 09:23:54 +0000

    Add  dropdown part for login or logout

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from , import views
+from .views import *
 
 urlpatterns = [
-    path('', view.index, name='home'),
+    path('', index, name='home'),
 ]
