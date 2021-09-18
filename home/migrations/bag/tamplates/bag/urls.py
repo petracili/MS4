6238ba991c8106e6dc410159f2056bc75b1496c0
petracili <petracili7@gmail.com>
2021-09-18 13:59:43 +0000

    Create urls part for bag

---
+++ home/migrations/bag/tamplates/bag/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.view_bag, name='view_bag'),
+    path('add/<plants_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<plants_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<plants_id>/', views.remove_from_bag, name='remove_from_bag'),
+]
