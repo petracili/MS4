f591d2dc0121c8695f270feae6c1270c7822732c
petracili <petracili7@gmail.com>
2021-09-18 10:37:16 +0000

    Added backgrounr part

---
+++ plants/migrations/__init__.py
@@ -0,0 +1,28 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Genre',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=254)),
+                ('friendly_name', models.CharField(blank=True, max_length=254, null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Plants',
+            fields=[
+                ('image', models.ImageField(blank=True, null=True, upload_to='')),
+                ('price', models.DecimalField(decimal_places=2, max_digits=6)),
+            ],
+        ),
+    ]
